2225d4f34e1738beba84663e64c474d69e69b089
Hass-seen <seennd1@gmail.com>
2023-01-04 21:36:27 +0200

    grabed the data

--- torch_Modle.py
+++ torch_Modle.py
@@ -3,18 +3,11 @@ import Data_set
 from torch import nn, save, load
 from torch.optim import Adam
 import os
+import numpy as np
 
 
 
 
-
-
-
-
-
-
-
-# Image Classifier Neural Network
 class PAD(nn.Module): 
     def __init__(self):
         super().__init__()
@@ -38,16 +31,30 @@ data=[]
 labels=[]
 
 real_path = r"D:\db\training_tesnsors\real_tensors"
+real_list = os.listdir(real_path)
+for real in real_list:
+    tensor = torch.load(real)
+    data.append(tensor)
+    labels.append(0)
+    
+hand_path = r"D:\db\training_tesnsors\hand_tensors"
+hand_list = os.listdir(hand_path)
+for hand in hand_list:
+    tensor = torch.load(hand)
+    data.append(tensor)
+    labels.append(1)
+    
+    
+fixed_path = r"D:\db\training_tesnsors\fixed_tensors"
+fixed_list = os.listdir(fixed_path)
+for fixed in fixed_list:
+    tensor = torch.load(fixed)
+    data.append(tensor)
+    labels.append(2)
 
-# Get a list of all the files in the folder
-file_list = os.listdir(folder_path)
-print(file_list)
-for vid in file_list:
+labels_tensor = torch.from_numpy(np.array(labels)).long()
 
-
-  
-
-dataset = Data_set(data, labels)
+dataset = Data_set(data, labels_tensor)
 
 # Create a data loader to iterate over the dataset
 dataloader = data.DataLoader(dataset, batch_size=200, shuffle=True)
